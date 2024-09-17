1a093f4f2273a8d6c4b014dd8b780eb9c5bf7edc
sarkanmagij <157028639+sarkanmagij@users.noreply.github.com>
2024-09-17 12:31:39 +0300

    Integrated Root Signals API into create_assistant function

---
+++ .venv/lib/python3.12/site-packages/root/models.py
@@ -0,0 +1,91 @@
+from functools import partial
+from typing import Iterator, Optional
+
+from root.generated.openapi_client.api.models_api import ModelsApi
+from root.generated.openapi_client.models.model import Model
+from root.generated.openapi_client.models.model_request import ModelRequest
+
+from .generated.openapi_client import ApiClient
+from .utils import iterate_cursor_list
+
+
+class Models:
+    """Models (sub) API
+
+    Note:
+
+      The construction of the API instance should be handled by
+      accesing an attribute of a :class:`root.client.RootSignals` instance.
+    """
+
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def list(self, *, limit: int = 100) -> Iterator[Model]:
+        """Iterate through the models.
+
+        Note:
+
+          The call will list only publicly available global models and
+          those models available to the organzation(s) of the user.
+
+        Args:
+          limit: Number of entries to iterate through at most.
+
+        """
+        api_instance = ModelsApi(self.client)
+        yield from iterate_cursor_list(partial(api_instance.models_list), limit=limit)
+
+    def create(
+        self,
+        *,
+        name: str,
+        model: Optional[str] = None,
+        default_key: Optional[str] = None,
+        max_output_token_count: Optional[int] = None,
+        max_token_count: Optional[int] = None,
+        url: Optional[str] = None,
+    ) -> str:
+        """Create a new model and return its ID.
+
+        Args:
+
+          name: The unique identifier for the model instance  (e.g. "google/gemma-2-9b").
+
+          model: The base model name to be used. Defaults to name.
+
+          default_key: The default API key required for the model, if applicable.
+
+          max_output_token_count: The maximum number of tokens to output.
+
+          max_token_count: The maximum number of tokens to process.
+
+          url: Optional URL pointing to the model's endpoint.
+
+        """
+        request = ModelRequest(
+            name=name,
+            model=model,
+            default_key=default_key,
+            max_output_token_count=max_output_token_count,
+            max_token_count=max_token_count,
+            is_local=url is not None,
+            url=url,
+        )
+
+        api_instance = ModelsApi(self.client)
+        return api_instance.models_create(model_request=request).id
+
+    def delete(self, model_id: str) -> None:
+        """
+        Delete the model.
+
+        Args:
+
+          model: The model to be deleted.
+
+        """
+        api_instance = ModelsApi(self.client)
+        return api_instance.models_destroy(id=model_id)
+
+    # TODO: update
---
+++ .venv/lib/python3.12/site-packages/root/objectives.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+from functools import partial
+from typing import TYPE_CHECKING, Iterator, List, Optional, cast
+
+from root.generated.openapi_client.api.objectives_api import ObjectivesApi
+from root.generated.openapi_client.models.evaluator_execution_functions_request import (
+    EvaluatorExecutionFunctionsRequest,
+)
+from root.generated.openapi_client.models.objective import Objective as OpenApiObjective
+from root.generated.openapi_client.models.objective_execution_request import ObjectiveExecutionRequest
+from root.generated.openapi_client.models.objective_list import ObjectiveList
+from root.generated.openapi_client.models.objective_request import ObjectiveRequest
+from root.generated.openapi_client.models.patched_objective_request import PatchedObjectiveRequest
+from root.generated.openapi_client.models.validator_execution_result import ValidatorExecutionResult
+
+from .generated.openapi_client import ApiClient
+from .skills import Skills
+from .utils import iterate_cursor_list
+
+if TYPE_CHECKING:
+    from .validators import Validator
+
+
+class Objective(OpenApiObjective):
+    """Wrapper for a single Objective.
+
+    For available attributes, please check the (automatically
+    generated) superclass documentation.
+    """
+
+    _client: ApiClient
+
+    @classmethod
+    def _wrap(cls, apiobj: OpenApiObjective, client: ApiClient) -> "Objective":
+        if not isinstance(apiobj, OpenApiObjective):
+            raise ValueError(f"Wrong instance in _wrap: {apiobj!r}")
+        obj = cast(Objective, apiobj)
+        obj.__class__ = cls
+        obj._client = client
+        return obj
+
+    def run(
+        self,
+        *,
+        response: str,
+        request: Optional[str] = None,
+        contexts: Optional[List[str]] = None,
+        functions: Optional[List[EvaluatorExecutionFunctionsRequest]] = None,
+    ) -> ValidatorExecutionResult:
+        """
+        Run all validators associated with the objective.
+
+        Args:
+
+          response: LLM output.
+
+          request: The prompt sent to the LLM. Optional.
+
+          contexts: Optional documents passed to RAG evaluators
+
+        """
+        api_instance = ObjectivesApi(self._client)
+        skill_execution_request = ObjectiveExecutionRequest(
+            request=request,
+            response=response,
+            contexts=contexts,
+            functions=functions,
+        )
+        return api_instance.objectives_objectives_execute_create(
+            objective_id=self.id, objective_execution_request=skill_execution_request
+        )
+
+
+class Objectives:
+    """Objectives API
+
+    Note:
+
+      The construction of the API instance should be handled by
+      accesing an attribute of a :class:`root.client.RootSignals` instance.
+    """
+
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def create(self, *, intent: Optional[str] = None, validators: Optional[List[Validator]] = None) -> Objective:
+        """Create a new objective and return its ID.
+
+        Args:
+
+          intent: The intent of the objective.
+
+          validators: An optional list of validators.
+
+        """
+        skills = Skills(self.client)
+        request = ObjectiveRequest(
+            intent=intent,
+            validators=[validator._to_request(skills) for validator in validators or []],
+        )
+        api_instance = ObjectivesApi(self.client)
+        objective = api_instance.objectives_create(objective_request=request)
+        return self.get(objective.id)
+
+    def get(self, objective_id: str) -> Objective:
+        """
+        Get an objective by ID.
+
+        Args:
+
+          objective_id: The objective to be fetched.
+
+        """
+        api_instance = ObjectivesApi(self.client)
+        return Objective._wrap(api_instance.objectives_retrieve(id=objective_id), self.client)
+
+    def delete(self, objective_id: str) -> None:
+        """
+        Delete the objective from the registry.
+
+        Args:
+
+          objective_id: The objective to be deleted.
+
+        """
+        api_instance = ObjectivesApi(self.client)
+        return api_instance.objectives_destroy(id=objective_id)
+
+    def list(self, *, intent: Optional[str] = None, limit: int = 100) -> Iterator[ObjectiveList]:
+        """Iterate through the objectives.
+
+        Note:
+
+          The call will list only publicly available global objectives and
+          those objectives available to the organzation(s) of the user.
+
+        Args:
+          intent: Specific intent the returned objectives must match.
+          limit: Number of entries to iterate through at most.
+
+        """
+        api_instance = ObjectivesApi(self.client)
+        yield from iterate_cursor_list(partial(api_instance.objectives_list, intent=intent), limit=limit)
+
+    def run(
+        self,
+        objective_id: str,
+        *,
+        response: str,
+        request: Optional[str] = None,
+        contexts: Optional[List[str]] = None,
+        functions: Optional[List[EvaluatorExecutionFunctionsRequest]] = None,
+    ) -> ValidatorExecutionResult:
+        """
+        Run all validators associated with an objective.
+
+        Args:
+
+          response: LLM output.
+
+          request: The prompt sent to the LLM. Optional.
+
+          contexts: Optional documents passed to RAG evaluators
+
+
+        """
+        api_instance = ObjectivesApi(self.client)
+        skill_execution_request = ObjectiveExecutionRequest(
+            request=request,
+            response=response,
+            contexts=contexts,
+            functions=functions,
+        )
+        return api_instance.objectives_objectives_execute_create(
+            objective_id=objective_id, objective_execution_request=skill_execution_request
+        )
+
+    def update(
+        self, objective_id: str, *, intent: Optional[str] = None, validators: Optional[List[Validator]] = None
+    ) -> Objective:
+        """
+        Update an existing objective.
+
+        Args:
+
+          objective_id: The objective to be updated.
+
+          intent: The intent of the objective.
+
+          validators: An optional list of validators.
+        """
+
+        skills = Skills(self.client)
+        request = PatchedObjectiveRequest(
+            intent=intent,
+            validators=[validator._to_request(skills) for validator in validators] if validators else None,
+        )
+        api_instance = ObjectivesApi(self.client)
+        return Objective._wrap(
+            api_instance.objectives_partial_update(id=objective_id, patched_objective_request=request),
+            client=self.client,
+        )
